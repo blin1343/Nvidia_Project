52487e5226070db35df264ba5bac8b79cd7d0b35
blin1343 <blin1343@gmail.com>
2023-12-04 20:32:04 -0500

    cleaned ui

--- overwatch.py
+++ overwatch.py
@@ -23,9 +23,9 @@ for url in urls:
         for d in data:
             metric = d.find_all("p", class_="name")
             value = d.find_all("p", class_="value")
+            # Scrape all data in the Averages table
             if 'Average' in d.find('p'):
                 for m, v in zip(metric,value):
-                    #print(n,m.text,v.text)
                     data_list.append({
                         "url": url,
                         "hero": heroes.text,
@@ -34,8 +34,70 @@ for url in urls:
                         "value": v.text
                     })
                 
-# Create DataFrame
-ow_data = pd.DataFrame(data_list)
+# Converts scraped data as a dataframe
+ow_data = pd.DataFrame(data_list) 
+# Extracts the unique blizzard profile from the url
 ow_data["url"] = ow_data["url"].str.split("/").str[-2]
-ow_data = ow_data[~ow_data["value"].str.contains(":")]
-ow_data.astype({'value': 'float'})
+
+# Filter for metrics most attributable to good performance
+ow_data = ow_data[ow_data["metric"].isin(["All Damage Done - Avg per 10 Min",
+                                          "Hero Damage Done - Avg per 10 Min",
+                                          "Eliminations - Avg per 10 Min",
+                                          "Final Blows - Avg per 10 Min",
+                                          "Deaths - Avg per 10 Min",
+                                          "Healing Done - Avg per 10 Min"])]
+
+# Converts value to a float
+ow_data['value'] = ow_data['value'].astype('float')
+
+# Adds a new column for the role a character falls into. (could not get this data from th career profile apge)
+def get_overwatch_role(character):
+    overwatch_roles = {
+        'Tracer': 'DPS',
+        'Reaper': 'DPS',
+        'Widowmaker': 'DPS',
+        'Pharah': 'DPS',
+        'Genji': 'DPS',
+        'Cassidy': 'DPS',
+        'Soldier: 76': 'DPS',
+        'Sombra': 'DPS',
+        'Bastion': 'DPS',
+        'Hanzo': 'DPS',
+        'Junkrat': 'DPS',
+        'Mei': 'DPS',
+        'Torbjörn': 'DPS',
+        'Sojourn': 'DPS',
+        'Symmetra': 'DPS',
+        'Ashe': 'DPS',
+        'Echo': 'DPS',
+        'Bob': 'Tank',
+        'D.Va': 'Tank',
+        'Orisa': 'Tank',
+        'Doomfist': 'Tank',
+        'Reinhardt': 'Tank',
+        'Roadhog': 'Tank',
+        'Sigma': 'Tank',
+        'Winston': 'Tank',
+        'Zarya': 'Tank',
+        'Junker Queen':'Tank',
+        'Ramattra':'Tank',
+        'Wrecking Ball':'Tank',
+        'Mauga':'Tank',
+        'Ana': 'Support',
+        'Baptiste': 'Support',
+        'Brigitte': 'Support',
+        'Lúcio': 'Support',
+        'Mercy': 'Support',
+        'Moira': 'Support',
+        'Zenyatta': 'Support',
+        'Kiriko': 'Support',
+        'Lifeweaver': 'Support',
+        'Illari': 'Support',
+    }
+    # Return the role for the given character
+    return overwatch_roles.get(character, 'Unknown')
+
+ow_data["role"] = ow_data["hero"].apply(get_overwatch_role)
+
+# Reorder the columns
+ow_data = ow_data[['hero_id','hero','role','url','metric','value']]
--- ow_streamlit.py
+++ ow_streamlit.py
@@ -1,37 +1,94 @@
 import pandas as pd
 import streamlit as st
+from streamlit_dynamic_filters import DynamicFilters
 import altair as alt
 import overwatch
  
 if __name__ == '__main__':
+
     ow_data = overwatch.ow_data
 
-    ow_data = ow_data[(ow_data['hero'] == 'Ashe') | (ow_data['hero'] == 'Ana' )]
+    # Row A: Set up page config, titles, and notes & assumptions
+    st.set_page_config(
+        page_title="Overwatch Stat Tracker",
+        page_icon="https://seeklogo.com/images/O/overwatch-logo-932A9F60A4-seeklogo.com.png",
+        layout="centered",
+        initial_sidebar_state="auto",
+        menu_items={
+            'About': "https://github.com/blin1343/Nvidia_Project"
+            }
+    )
 
-    ow_data
+    tab1, tab2 = st.tabs(["Dashboard", "Data"])
 
-    st.markdown("---")
-    label = pd.DataFrame({"Column 1": [1,2,3,4,5]})
-    st.markdown("<h1 style='text-align: center; color: black;'>Benny is better than Syd at Overwatch</h1>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center; color: grey;'>See proof below :) </h4>", unsafe_allow_html=True)
+    with tab1:
+        #col1, col2, = st.columns([1,1])
+        st.image('https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Overwatch_2_text_logo.svg/2560px-Overwatch_2_text_logo.svg.png',width=700)
+        st.markdown("<h1 style='text-align: center; color: black;'>Stat Tracker</h1>", unsafe_allow_html=True)
+        st.markdown("---")
 
-    # Sidebar
-    st.sidebar.header('Filters')
+        st.text_area(
+            "***Notes & Assumptions***",
+            "* Data is scraped from Blizzard's offical OW2 career profile page\n"
+            "* Data will not include player's with profiles set to private\n"
+            "\n"
+            "Metric Defintions:\n"
+            "* All Damage Done - Avg. damage done to other players (including shields & objects) per 10 minutes\n"
+            "* Final Blows - Avg. number of eliminations where the player landed the final blow per 10 minutes\n"
+            "* Deaths - Avg. number of deaths per 10 minutes\n"
+            "* Hero Damage Done - Avg. damage done to other players (excluding shields & objects) per 10 minutes\n"
+            "* Eliminations - Avg. number of eliminations that involved the player per 10 minutes\n"
+            "* Healing Done - Avg. amount healed per 10 minutes (support role can only heal)",
+            height = 200
+        )
 
-    # Select box for filtering data based on a category
-    #selected_hero = st.sidebar.selectbox('Select a Hero:', ow_data['hero'].unique())
-    #st.header(f'Data for Hero: {selected_hero}')
-    selected_metric = st.sidebar.selectbox('Select a Metric:', ow_data['metric'].unique())
+        st.write(f"\n")
 
-    # Filter the data based on user selections
-    filtered_df = ow_data[ow_data['metric'] == selected_metric]
+        # Row B: Sidebar & Filters
+        st.sidebar.header('Filters')
 
-    chart=alt.Chart(filtered_df).mark_bar(strokeWidth=100).encode(
-    x=alt.X('url:N', title="", scale=alt.Scale(paddingOuter=0.5)),#paddingOuter - you can play with a space between 2 models 
-    y='value:Q',
-    color='hero:N',
-    column=alt.Column('hero:N', title="", spacing =0), #spacing =0 removes space between columns, column for can and st 
-    ).properties( width = 300, height = 300, ).configure_header(labelOrient='bottom').configure_view(
-        strokeOpacity=0)
+        all_heroes = sorted(ow_data['hero'].unique())
+        all_roles = sorted(ow_data['role'].unique())
+        all_metrics = sorted(ow_data['metric'].unique())
 
-    st.altair_chart(chart, theme="streamlit") 
+        role_filter = st.sidebar.selectbox('Select Role', all_roles)
+        hero_filter = st.sidebar.multiselect('Select Hero (Multi-Select)', sorted(ow_data[ow_data['role'] == role_filter]['hero'].unique()))
+        metric_filter = st.sidebar.selectbox('Select Metric', sorted(ow_data[ow_data['role'] == role_filter]['metric'].unique()))
+
+       # Row C: Top Level Metrics
+        elims = ow_data[(ow_data['metric'] == 'Eliminations - Avg per 10 Min') & (ow_data['role'] == role_filter)].groupby(['role'])['value'].mean()
+        healing = ow_data[(ow_data['metric'] == 'Healing Done - Avg per 10 Min') & (ow_data['role'] == role_filter)].groupby(['role'])['value'].mean()
+        deaths = ow_data[(ow_data['metric'] == 'Deaths - Avg per 10 Min') & (ow_data['role'] == role_filter)].groupby(['role'])['value'].mean()
+
+        # Check if the multiselect is empty
+        if not hero_filter:
+            st.warning("Please select a hero(s) to get started.")
+        else:
+            col1, col2, col3 = st.columns(3)
+            col1.metric("Eliminations - Avg per 10 Min ", round(elims, 2))
+            col2.metric("Healing Done - Avg per 10 Min", round(healing, 2))
+            col3.metric("Deaths - Avg per 10 Min", round(deaths, 2))
+            
+            st.markdown("---")
+
+            filtered_df = ow_data[(ow_data['role'] == role_filter) & (ow_data['hero'].isin(hero_filter)) & (ow_data['metric'] == metric_filter)]
+
+            st.write(f"Comparing the {metric_filter} metric")
+            chart = alt.Chart(filtered_df).mark_bar().encode(
+                x=alt.X('value:Q',title=""),
+                y=alt.Y('url', title=""),
+                color='url:N',
+                row='hero:N',
+                tooltip=['hero', 'role', 'url', 'value']
+            ).properties( autosize=alt.AutoSizeParams
+                ( type="fit",
+                resize=True ,
+                contains="padding"
+                )
+            )
+                
+            st.altair_chart(chart, theme="streamlit") 
+
+
+    with tab2:
+        st.dataframe(ow_data, use_container_width=True)
