a29ca544e6288a5c13a24f53ebc1300afe8613c6
blin1343 <blin1343@gmail.com>
2023-12-04 21:53:53 -0500

    fixed bugs

--- overwatch.py
+++ overwatch.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 import requests
-import itertools 
+#import itertools 
 
 urls = ["https://overwatch.blizzard.com/en-us/career/takaharimi-1252/",
        "https://overwatch.blizzard.com/en-us/career/blin1343-1104/"
